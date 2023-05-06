748bdf1f1368b3831b51f5ed907a2360c7fc0e03
an-zaz <anzazerskaya@gmail.com>
2023-05-06 19:11:34 +0400

    feat: finish task 03.01, 03.02

--- src/app.ts
+++ src/app.ts
@@ -52,14 +52,14 @@ function getAllBooks(): readonly Book[] {
 //     const title: string = books.find((book) => book.available).title;
 // }
 
-function logFirstAvailable( books: readonly Book[]): void {
+function logFirstAvailable( books: readonly Book[] = getAllBooks()): void {
     console.log(`Num of books: ${books.length}`);
 
     const title: string | undefined = books.find(({available}) => available)?.title;
     console.log(`1st available book: ${title}`);
 }
 
-function getBookTitlesByCategory(category: Category): Array<string>{
+function getBookTitlesByCategory(category: Category = Category.JavaScript): Array<string>{
     const books = getAllBooks();
 
     const titles: string[] = books.filter((book) => book.category === category)
@@ -93,9 +93,74 @@ function calcTotalPages(): void {
     }, 0n);
 }
 
+function createCustomerID(name: string, id: number) {
+    return `${name} - ${id}`;
+}
+
+// city will stay optional as it goes after optional argument
+function createCustomer(name: string, age?: number, city: string = 'Kiev') {
+    console.log(`Customer ${name}`);
+
+    if (age) {
+        console.log(`Customer ${age}`);
+    }
+
+    if (city) {
+        console.log(`Customer ${city}`);
+    }
+}
+
+function getBookByID(id: number): Book {
+    const books = getAllBooks();
+
+    return books.find(book => book.id === id);
+}
+
+function сheckoutBooks(customer: string, ...bookIds: number[]): string[]{
+    console.log(`Customer: ${customer}`);
+
+    return bookIds
+        .map( id => getBookByID(id))
+        .filter( book => book.available)
+        .map(({title}) => title);
+}
+
 // Task 02.01, 02.02
 logFirstAvailable(getAllBooks());
 logBookTitles(getBookTitlesByCategory(Category.JavaScript));
 console.log(getBookAuthorByIndex(0));
 console.log(getBookAuthorByIndex(10));
 
+// Task 03.01
+const myId: string = createCustomerID('Ann', 10);
+console.log(myId);
+
+const a = typeof 10;  // a = 'number'
+
+// let idGenerator: (name: string, id: number) => string;
+// or
+let idGenerator: typeof createCustomerID;
+idGenerator = (name2: string, id: number) => `${name2} - ${id}`;
+const myId2: string = idGenerator('Ann', 10);
+console.log(myId2);
+
+idGenerator = createCustomerID;
+const myId3: string = idGenerator('Ann', 10);
+console.log(myId3);
+
+// Task 03.02
+createCustomer('Anna');
+createCustomer('Anna', 25);
+createCustomer('Anna', 35, 'Budapesht');
+
+const args: [string, number, string] = ['Kate', 25, 'Budva'];
+// or
+// const args: Parameters<typeof createCustomer> = ['Kate', 25, 'Budva'];
+createCustomer(...args);
+
+console.log(getBookTitlesByCategory());
+logFirstAvailable();
+console.log(getBookByID(10));
+
+const myBooks = сheckoutBooks('Ann',1,2,3,4);
+console.log(myBooks);
